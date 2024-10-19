68b91d1f4a829260149a1adbfadfe1bee3366d8b
Kuron <1157078+Kuron@users.noreply.github.com>
2024-10-19 19:36:50 +0100

    chore: add lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,23 @@ import pluginJs from "@eslint/js";
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    rules: {
+      'camelcase': ['error'],
+      'complexity': ['warn'],
+      'max-depth': ['warn'],
+      'max-params': ['warn', 4],
+      'max-statements': ['warn', 20],
+      'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
+      'no-eval': ['error'],
+      'no-inner-declarations': ['error'],
+      'no-label-var': ['error'],
+      'no-labels': ['error'],
+      'no-lone-blocks': ['error'],
+      'no-lonely-if': ['error'],
+      'no-param-reassign': ['error'],
+      'no-var': ['error'],
+      'no-warning-comments': ['warn'],
+    }
+  },
 ];
