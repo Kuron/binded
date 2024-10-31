8f275a540f17f35c24d6f9c00c5eee421ba6f82f
Kuron <1157078+Kuron@users.noreply.github.com>
2024-10-31 09:17:39 -0400

    fix: fix error

--- src/index.mjs
+++ src/index.mjs
@@ -7,7 +7,7 @@ const profilingStats = {};
 window.binded = {
   bind: (elem, opts) => {
     const ret = (opts?.profiling ? profileAllMethods(binded, profilingStats) : binded).init(elem, opts);
-    opts.profiling && console.table(profilingStats);
+    opts?.profiling && console.table(profilingStats);
     return ret;
   },
   register: () => { throw new Error('binded: Not implemented'); },
